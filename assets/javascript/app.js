54b1f21d4568e0c07e67a5122d6e3388240d24bb
Kianimal <spoono17@gmail.com>
2020-02-26 21:19:29 -0600

    Minor text styling changes. Added radio buttons for question 1. Going to change to a function that adds radio buttons with answer text in a more dynamic, modular way. Questions stored in an array in object format. No timer yet. Need to limit to one answer per question - can currently select all buttons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,17 @@ var rightCount = 0;
 var wrongCount = 0;
 var j = 0;
 
+var questions = [
+    {
+        textVal: "Which Star Wars movie was the first to be released?",
+        right: "Episode IV - A New Hope",
+        a: "<input class='btnAnswer' type='radio' name='a' value='Episode IV - A New Hope'> <span class='txtAnswer'>Episode IV - A New Hope</span>",
+        b: "<input class='btnAnswer' type='radio' name='b' value='Episode V - The Empire Strikes Back'> <span class='txtAnswer'>Episode V - The Empire Strikes Back</span>",
+        c: "<input class='btnAnswer' type='radio' name='c' value='Episode I - The Phantom Menace'> <span class='txtAnswer'>Episode I - The Phantom Menace</span>",
+        d: "<input class='btnAnswer' type='radio' name='d' value='Kinect Star Wars for Xbox 360'> <span class='txtAnswer'>Kinect Star Wars for Xbox 360</span>"
+    }
+];
+
 var qHead = document.getElementById("qHeader");
 var qHr = document.getElementById("qHr");
 var qBody = document.getElementById("qBody")
@@ -17,5 +28,21 @@ $(".btn").click(function(){
     qHead.textContent = "Question " + (questionNum[j]+1);
     $(qBody).empty(), $(qBody).append(qHead), 
     $(qBody).append(qHr);
+    var question = document.createElement("div");
+    question.classList.add("questionText");
+    question.textContent = questions[j].textVal;
+    $(qBody).append(question);
+    var a = questions[j].a;
+    var b = questions[j].b;
+    var c = questions[j].c;
+    var d = questions[j].d;
+    var br = "<br>"
+    $(qBody).append(a);
+    $(qBody).append(br);
+    $(qBody).append(b);
+    $(qBody).append(br);
+    $(qBody).append(c);
+    $(qBody).append(br);
+    $(qBody).append(d);
     j++;
 });
