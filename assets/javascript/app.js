b03ea28658082942a102c65b0f2c0b57b13a1ebe
Kianimal <spoono17@gmail.com>
2020-02-27 01:30:19 -0600

    Radio buttons work as intended. Function to populate questions implemented.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,16 @@ var questions = [
     {
         textVal: "Which Star Wars movie was the first to be released?",
         right: "Episode IV - A New Hope",
-        a: "<input class='btnAnswer' type='radio' name='a' value='Episode IV - A New Hope'> <span class='txtAnswer'>Episode IV - A New Hope</span>",
-        b: "<input class='btnAnswer' type='radio' name='b' value='Episode V - The Empire Strikes Back'> <span class='txtAnswer'>Episode V - The Empire Strikes Back</span>",
-        c: "<input class='btnAnswer' type='radio' name='c' value='Episode I - The Phantom Menace'> <span class='txtAnswer'>Episode I - The Phantom Menace</span>",
-        d: "<input class='btnAnswer' type='radio' name='d' value='Kinect Star Wars for Xbox 360'> <span class='txtAnswer'>Kinect Star Wars for Xbox 360</span>"
+        a: "Episode IV - A New Hope",
+        b: "Episode V - The Empire Strikes Back",
+        c: "Episode I - The Phantom Menace",
+        d: "Kinect Star Wars for Xbox 360"
     }
 ];
+
+function populateQuestion(answer){
+    return ("<input class='btnAnswer' type='radio' name='a' value='" + answer + "'> <span class='txtAnswer'>" + answer + "</span><br>");
+};
 
 var qHead = document.getElementById("qHeader");
 var qHr = document.getElementById("qHr");
@@ -25,24 +29,24 @@ for (i=0;i<questionCount;i++){
 }
 
 $(".btn").click(function(){
+    
     qHead.textContent = "Question " + (questionNum[j]+1);
     $(qBody).empty(), $(qBody).append(qHead), 
     $(qBody).append(qHr);
+
     var question = document.createElement("div");
     question.classList.add("questionText");
     question.textContent = questions[j].textVal;
+
     $(qBody).append(question);
-    var a = questions[j].a;
-    var b = questions[j].b;
-    var c = questions[j].c;
-    var d = questions[j].d;
-    var br = "<br>"
+    var a = populateQuestion(questions[j].a);
     $(qBody).append(a);
-    $(qBody).append(br);
+    var b = populateQuestion(questions[j].b);
     $(qBody).append(b);
-    $(qBody).append(br);
+    var c = populateQuestion(questions[j].c);
     $(qBody).append(c);
-    $(qBody).append(br);
+    var d = populateQuestion(questions[j].d);
     $(qBody).append(d);
+
     j++;
 });
